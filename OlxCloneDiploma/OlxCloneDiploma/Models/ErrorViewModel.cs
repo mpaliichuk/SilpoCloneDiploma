cc280a5a6f03c559eeb7c9c2f3ee74a04873c463
mpaliichuk <marian.paliichuk@gmail.com>
2024-06-03 21:54:02 +0300

    first commit

---
+++ OlxCloneDiploma/OlxCloneDiploma/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OlxCloneDiploma.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
