7e92a8fb91d762fda0b3d24055c52d0e79acfd83
tarasveretilnyk <veretilnuktar@gmail.com>
2024-07-08 23:29:00 +0200

    Recreate project for frontend

--- SilpoCloneDiploma/SilpoCloneDiploma/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SilpoCloneDiploma.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
