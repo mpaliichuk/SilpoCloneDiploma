ef9663845783403a051b8ed0ae73b26bb3b429fb
mpaliichuk <marian.paliichuk@gmail.com>
2024-08-12 14:00:41 +0300

    Added product page, still needs improvement

---
+++ SilpoCloneDiploma/FrontEnd/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FrontEnd.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // You can pass any default data or simply return the view
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
