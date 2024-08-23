467730a8cc5732153352300945624eea8c619d77
mpaliichuk <marian.paliichuk@gmail.com>
2024-08-24 00:41:59 +0300

    Created FrontEnd for ShoppingCart

---
+++ SilpoCloneDiploma/FrontEnd/Controllers/ShoppingCartController.cs
@@ -0,0 +1,27 @@
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FrontEnd.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        private readonly ILogger<ShoppingCartController> _logger;
+
+        public ShoppingCartController(ILogger<ShoppingCartController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // You can pass any default data or simply return the view
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
