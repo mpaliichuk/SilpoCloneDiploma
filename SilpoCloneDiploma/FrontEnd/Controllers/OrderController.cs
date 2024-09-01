aae278123cd7bae8b2bbc55fd43b55b9ec398de6
mpaliichuk <marian.paliichuk@gmail.com>
2024-09-01 16:43:47 +0300

    Made minimal frontend for OrderPage

---
+++ SilpoCloneDiploma/FrontEnd/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+using FrontEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FrontEnd.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        public OrderController(ILogger<OrderController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // You can pass any default data or simply return the view
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
